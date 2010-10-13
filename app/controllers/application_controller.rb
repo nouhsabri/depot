e347f559ba9450a8ed0e3dff73bf72c06ab69f6f
nouh <nooh_fci@hotmail.com>
2010-10-13 16:29:10 -0700

    New update ..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 #---
 # Excerpted from "Agile Web Development with Rails, 4rd Ed.",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class ApplicationController < ActionController::Base
   before_filter :set_i18n_locale_from_params
   # ...
-  before_filter :authorize
+#  before_filter :authorize
   protect_from_forgery
 
   private
 
-    def current_cart 
+    def current_cart
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       cart = Cart.create
@@ -24,25 +24,25 @@ class ApplicationController < ActionController::Base
 
     # ...
 
-  protected
-    def authorize
-      if request.format == Mime::HTML 
-        unless User.find_by_id(session[:user_id])
-          redirect_to login_url, :notice => "Please log in"
-        end
-      else
-        authenticate_or_request_with_http_basic do |username, password|
-          User.authenticate(username, password)
-        end
-      end
-    end
+ # protected
+  #  def authorize
+   #   if request.format == Mime::HTML
+    #    unless User.find_by_id(session[:user_id])
+     #     redirect_to login_url, :notice => "Please log in"
+      #  end
+      #else
+       # authenticate_or_request_with_http_basic do |username, password|
+        #  User.authenticate(username, password)
+        #end
+      #end
+    #end
 
     def set_i18n_locale_from_params
       if params[:locale]
         if I18n.available_locales.include?(params[:locale].to_sym)
           I18n.locale = params[:locale]
         else
-          flash.now[:notice] = 
+          flash.now[:notice] =
             "#{params[:locale]} translation not available"
           logger.error flash.now[:notice]
         end
@@ -54,3 +54,4 @@ class ApplicationController < ActionController::Base
       { :locale => I18n.locale }
     end
 end
+
